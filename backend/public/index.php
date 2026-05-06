22b8772d03593229a81cae8d62ed6486735e58ba
Reeyan Faife <reeyanfaife61@gmail.com>
2026-05-06 12:52:24 +0200

    Simplify backend Dockerfile and fix db.php include

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-include __DIR__ . '/../db.php';
+include 'db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
